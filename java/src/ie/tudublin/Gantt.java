454973834e39e23f33ea3e074b292d312e8389f4
hanyueshi <shihanyue749@gmail.com>
2020-04-22 19:32:13 +0100

    load and print

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,25 +1,42 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
+	ArrayList<Task> task = new ArrayList<Task>();
+	// ArrayList<Task> start = new ArrayList<Task>();
+	// ArrayList<Task> end = new ArrayList<Task>();
 	
 	public void settings()
 	{
 		size(800, 600);
 	}
 
+
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv","header");
+
+		for (TableRow row:table.rows()){
+
+			Task t = new Task(row);
+			task.add(t);
+		}
 	}
 
 	public void printTasks()
 	{
+		for (Task t:task){
+			System.out.println(t);
+		}
 		
 	}
-	
+
 	public void mousePressed()
 	{
 		println("Mouse pressed");	
@@ -34,6 +51,8 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
